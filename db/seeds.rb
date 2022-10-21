c9c2bf9e374594e7dbe9b9b1fbe11db19ff350d7
Minori411 <whmd.030@icloud.com>
2022-10-21 16:51:45 +0900

    seed news add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,28 @@ user = User.new(id: 1,
                 userstyle: 0)
 user.save!
 
-10.times do |n|
+5.times do |n|
     User.create!(id: n + 2,
                  name: Gimei.kanji,
+                 email: "admin1234#{n + 1}@sample.com",
+                 password: "admin1234#{n + 1}",
+                 userstyle: 0
+                 )
+end
+
+6.times do |n|
+    News.create!(title: "GASIN",
+                 body: "This week, GASIN is gonna be held.",
+                 user_id: n + 1
+    )
+end
+
+10.times do |n|
+    User.create!(id: n + 7,
+                 name: Gimei.kanji,
                  email: "worker1234#{n + 1}@hoge.com",
-                 password: "minori1234#{n + 1}",
+                 password: "worker1234#{n + 1}",
                  userstyle: 1
                  )
-  end
+end
+
