cf63fcd512c9b87a87ae2577b24a5ade6290c2b2
Minori411 <whmd.030@icloud.com>
2022-10-24 23:07:58 +0900

    week

--- app/javascript/packs/schedule.js
+++ app/javascript/packs/schedule.js
@@ -1,63 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
+var calendarEl = document.getElementById('calendar');
+var calendar = new Calendar(calendarEl, {
+    timeZone: 'UTC',
+    events: [
+      {
+        id: 'a',
+        title: 'my event',
+        start: '2022-10-01'
+      }
+    ]
+  })
   
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      initialDate: '2022-08-07',
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2022-08-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2022-08-07',
-          end: '2022-08-10'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2022-08-09T16:00:00'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2022-08-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2022-08-11',
-          end: '2022-08-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2022-08-12T10:30:00',
-          end: '2022-08-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2022-08-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2022-08-12T14:30:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2022-08-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2022-08-28'
-        }
-      ]
-    });
-  
-    calendar.render();
-  });
+  var event = calendar.getEventById('a') // an event object!
+  var start = event.start // a property (a Date object)
+  console.log(start.toISOString()) // "2018-09-01T00:00:00.000Z"
+  calendar.render();
