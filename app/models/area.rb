56df3d308288a58ea7f5835f229bd0ce15c75f73
Minori411 <whmd.030@icloud.com>
2022-11-04 11:26:09 +0900

    jgc

--- app/models/area.rb
+++ app/models/area.rb
@@ -3,6 +3,6 @@ class Area < ApplicationRecord
     has_many :news_area_sections, dependent: :destroy
     has_many :news, dependent: :destroy
     has_many :user_area_sections, dependent: :destroy
-    enum :area_name, { A_Processarea: 1, B_Utilityarea: 2,  C_offsite: 3, Allarea: 99}
+    enum :area_name, { A_Processarea: 1, B_Utilityarea: 2,  C_Offsite: 3, Allarea: 99}
 
 end
